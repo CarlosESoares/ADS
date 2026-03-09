5832f732db5ffc4f1e002463d5d01972dd051086
carlos <soares>
2026-03-09 18:41:02 -0300

    Teste

--- POO/src/heranca/Loja.java
+++ POO/src/heranca/Loja.java
@@ -2,8 +2,13 @@ package heranca;
 public class Loja {
 	public static void main(String[] args) {
 		Roupa camisa = new Roupa();
+		camisa.setNome("camisa polo");
+		camisa.setPreco(20);
+		camisa.setTamanho("14");
 		
-		camisa.
+		
+		
+		
 		
 	}
 }
