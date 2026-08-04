683b29b1aa2734aa34b9cbd17fcc2ffebb256261
Carlos <Soares>
2026-08-03 21:53:54 -0300

    Contador

--- app/src/main/java/com/ifsc/app/MainActivity.java
+++ app/src/main/java/com/ifsc/app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ifsc.app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    int a = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        TextView tv = findViewById(R.id.text);
+        Button bt = findViewById(R.id.button);
+        tv.setText("--");
+        bt.setText("Click aqui");
+        bt.setOnClickListener(view -> {
+            a++;
+            bt.setText("Ui");
+            tv.setText(Integer.toString(a));
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
 }
