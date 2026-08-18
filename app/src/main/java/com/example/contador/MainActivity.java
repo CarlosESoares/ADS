c3251ca2aaa71bf6dc6c95ab523a3f50f81ff322
CArlos <soares>
2026-08-17 21:06:46 -0300

    Contador Real

---
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -0,0 +1,44 @@
+
+package com.example.contador;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    TextView textView2;
+    Button bt;
+    int contador=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        bt = findViewById(R.id.bt);
+        textView = findViewById(R.id.tv);
+        textView2 = findViewById(R.id.tv2);
+
+
+        bt.setOnClickListener( v->{
+            textView.setText("FRITAS!!:" +Integer.toString(contador));
+            contador +=1;
+            textView2.setText("BLÁ");
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
