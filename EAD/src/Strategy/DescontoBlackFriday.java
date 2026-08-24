406974ef2ca82922d294867f95d747f90cdf172b
Carlos <soares>
2026-08-24 19:41:07 -0300

    Nova Branch

--- EAD/src/Strategy/DescontoBlackFriday.java
+++
@@ -1,7 +0,0 @@
-package Strategy;
-public class DescontoBlackFriday implements EstrategiaDesconto {
-    @Override
-    public double aplicarDesconto(double valor) {
-        return valor * 0.60;
-    }
-}
--- EAD/src/factory/Triangulo.java
+++
@@ -1,9 +0,0 @@
-package factory;
-public class Triangulo implements Forma {
-    @Override
-    public double calcularArea(double... dimensoes) {
-        double base = dimensoes[0];
-        double altura = dimensoes[1];
-        return (base * altura) / 2;
-    }
-}
